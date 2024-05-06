92b1b2a86f3ca7a921c1143c427934d2ec1570a9
Sean Banko <sbanko@crusoeenergy.com>
2024-05-05 22:09:38 -0700

    better sessions

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -4,7 +4,14 @@ return {
   keys = {
     { "<leader>ls", function() require("auto-session.session-lens").search_session() end, mode = "n", silent = true },
   },
+  dependencies = {
+    "nvim-neo-tree/neo-tree.nvim",
+  },
   config = function()
-    require("auto-session").setup()
+    vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+    require('auto-session').setup {
+      pre_save_cmds = { "Neotree close" },
+      post_restore_cmds = { "Neotree action=show source=filesystem reveal=true" },
+    }
   end
 }
