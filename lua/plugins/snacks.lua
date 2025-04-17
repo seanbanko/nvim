cd197ab25df3b945097da4ff0bc0fa0e94f8a237
Sean Banko <sean.banko@gmail.com>
2025-04-17 07:57:44 -0700

    update keymap for filetree

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
     terminal = { enabled = true },
   },
   keys = {
-    { "<leader>e",  function() Snacks.explorer() end,  desc = "File Explorer" },
+    { "<leader>nt", function() Snacks.explorer() end,  desc = "File Explorer" },
     { "<leader>go", function() Snacks.gitbrowse() end, desc = "Git Browse",   mode = { "n", "v" } },
     { "<leader>t",  function() Snacks.terminal() end,  desc = "Terminal",     mode = "n" },
   }
