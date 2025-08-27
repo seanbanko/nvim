848bff440950345d59d955c264f6e8150ca5a552
Sean Banko <sean.banko@gmail.com>
2025-08-26 20:14:04 -0700

    feat: use prettier for typescript

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -8,6 +8,8 @@ return {
       markdown = { "prettier" },
       rust = { "rustfmt" },
       terraform = { "terraform_fmt" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
     },
     format_on_save = {
       lsp_fallback = true,
